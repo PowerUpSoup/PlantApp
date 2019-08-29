0e12166b3eccc8f7523b7d4c7287947478ce159d
Sean Jackson <PowerUpSoup@gmail.com>
2019-08-29 09:16:01 -0700

    Add files via upload
    Added a javascript file with an outline of the functions required for full functionality.
    
    Functions required for Minimum viable product:
    - 1 search function
    - GET request to trefle API
    - populate search field based on search function
    - display plant care instructions based on user selection

---
+++ index.js
@@ -0,0 +1,44 @@
+function instructionsCloseEventListener() {
+
+}
+
+function displayPlantCareInstructions() {
+
+}
+
+function searchResultsEventListener() {
+
+}
+
+function formatSearchParameters() {
+
+}
+
+function makeRequestToTrefle() {
+
+}
+
+function populateSearchResults() {
+
+}
+
+function userInputEventListener() {
+    
+}
+
+function plantListEventListener() {
+    
+}
+
+function alphaEventListener() {
+   
+}
+
+function watchform() {
+    
+}
+
+$(function() {
+    console.log('App loaded and waining for user input!');
+    watchForm();
+} )
